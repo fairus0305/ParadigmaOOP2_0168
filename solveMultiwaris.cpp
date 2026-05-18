26c551c9e01fe95496956c278725214bfd57bf26
fairus0305 <ruszezzy@gmail.com>
2026-05-18 15:04:39 +0700

    memebuat class pekerja dengan warisan dari orang

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -11,3 +11,13 @@ class orang {
             cout << " orang di buat dengan umur " << umur << "\n" << endl;
         }
 };
+
+class pekerja : public orang {
+public :    
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja di buat\n"<< endl;
+    }
+};
+
