a026c63640c356ce3a0ee9a2ff25dd3f4f51bd82
fairus0305 <ruszezzy@gmail.com>
2026-05-18 15:12:21 +0700

    menambah method setaklarno

--- enkasulapsi.cpp
+++ enkasulapsi.cpp
@@ -7,5 +7,12 @@ private:
     string saklarno[5];
 
 public:
-    
+    void setsaklarno(int i, string value)
+    {
+        saklarno[i] = value;
+    }
+    string getsaklarno(int i)
+    {
+        return saklarno[i];
+    }
 };
