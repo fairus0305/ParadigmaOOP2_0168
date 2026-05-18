889671293afe548aaea004c2335b92f32e924e28
fairus0305 <ruszezzy@gmail.com>
2026-05-18 15:14:18 +0700

    membuat pemanggilan saklar dengan get

--- enkasulapsi.cpp
+++ enkasulapsi.cpp
@@ -26,7 +26,10 @@ int main()
     lampurumah.setsaklarno(2, "Lampu kamar Tidur");
     lampurumah.setsaklarno(3, "Lampu Dapur");
 
-    
+    cout << lampurumah.getsaklarno(0) << endl;
+    cout << lampurumah.getsaklarno(1) << endl;
+    cout << lampurumah.getsaklarno(2) << endl;
+    cout << lampurumah.getsaklarno(3) << endl;
 
     return 0;
 }
