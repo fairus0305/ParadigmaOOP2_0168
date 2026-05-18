d0115a1b45dd5980db9e8cc76c6d30ae827c5b81
fairus0305 <ruszezzy@gmail.com>
2026-05-18 15:07:55 +0700

    membuat constructor orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -5,6 +5,10 @@ class orang
 {
 public:
     int umur;
-    
+    orang(int pUmur) : umur(pUmur)
+    {
+        cout << " orang di buat dengan umur " << umur << "\n"
+             << endl;
+    }
 };
 
