edd3fb35dbe22572fa3a80773cbc409b35ec9194
fairus0305 <ruszezzy@gmail.com>
2026-05-18 15:20:01 +0700

    membuat class baseClass

---
+++ overriding.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class baseClass
+{
+public:
+    virtual void perkenalan() final
+    {
+        cout << "Halo saya function dari base class";
+    }
+};
