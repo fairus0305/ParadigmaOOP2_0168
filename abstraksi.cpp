e0108db27d6f3db843f98b646e8d351885e46958
fairus0305 <ruszezzy@gmail.com>
2026-05-18 15:16:03 +0700

    menambah method untuk mengisi nilai

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -6,5 +6,18 @@ class AbstraksiKlas
 private:
     string x, y;
 
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
+    void display()
+    {
+        cout << "X = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
 
-}
